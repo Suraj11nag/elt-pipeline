a54b9d72974e5d43943387acf74d96599a083ce1
Suraj Nag <suraj11nag@gmail.com>
2024-08-23 23:26:42 +0530

    minor update

--- elt/script.py
+++ elt/script.py
@@ -4,3 +4,5 @@ import time
 def wait_for_postgres(host, max_retries=5, delay_seconds=5):
     """Wait for PostgreSQL to become available."""
     retries = 0
+    while retries < max_retries:
+        try:
